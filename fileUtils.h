7f006fb94650660a94355f90672ccc14c919ea9e
Goobrr <ggoobbrr@gmail.com>
2021-11-17 15:39:40 +0700

    """improve""" fileUtils

--- fileUtils.h
+++ fileUtils.h
@@ -1,61 +1,62 @@
-#ifndef FILEUTILS_H
-#define FILEUTILS_H
+#pragma once
 
 #include <iostream>
 #include <fstream>
-#include <bitset>
 #include <string>
 
 class Writer {
-    public:
+    private:
         std::ofstream f;
+        
+    public:
         int length = 0;
 
         Writer(char *filename){
             f.open(filename, std::ios::binary);
         }
 
+        // TODO specialize strings
         template <typename T>
         void write(T value){;
             f.write(reinterpret_cast<char *>(&value), sizeof(T));
+
             length += sizeof(T);
         }
-
-        // compiler thinks this is is a primary template and not a template specialisation
-        /*
-        template <typename T>
-        void write<std::string>(std::string text){;
-        }
-        */
 
         int close(){
             f.close();
             std::cout << "wrote " << length << " bytes\n";
+
             return length;
         }
 };
 
 class Reader {
-    public:
+    private:
         std::ifstream f;
         char* buffer = new char[8];
+
+    public:
+        int count = 0;
 
         Reader(char *filename){
             f.open(filename, std::ios::binary);
         }
 
+        // TODO specialize strings
         template <typename T>
         T read(){
             T res;
             f.read(reinterpret_cast<char *>(&res), sizeof(T));
+            count += sizeof(T);
 
             return res;
         }
 
-        void close(){
+        int close(){
             f.close();
+            std::cout << "read " << count << " bytes\n";
+
+            return count;
         }
 };
-
-
-#endif
